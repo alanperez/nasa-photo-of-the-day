d4becec80a8e091fa67f259c76d82e68c0c4f337
Alan Perez <alanp9six@gmail.com>
2019-07-11 16:05:54 -0500

    displays data

--- src/components/Data.js
+++ src/components/Data.js
@@ -1,4 +1,6 @@
 import React,{ useState, useEffect } from 'react'
+import Typography from '@material-ui/core/Typography';
+import CardMedia from '@material-ui/core/CardMedia';
 import axios from 'axios';
 
 const Data = () => {
@@ -7,20 +9,24 @@ const [data,setData] = useState('')
 
   useEffect(() => {
      axios.get('https://api.nasa.gov/planetary/apod?api_key=FUl9H67678sb9kCudx3rZUeyrhwTCebr1657dwPU').then(res => setData(res.data))
-
+      
     // setPhotos(res.data);
   }, []);
 
+  
   
   return (
     <>
     <div className="content" style={{textAlign:'center', margin: '50px'}}>
       <div className="title">
-        {data.title}
-        <p>
-          {data.explanation}
-        </p>
-        <img src={data.url} alt={data.title} />
+        <Typography variant="h2">{data.title}</Typography>
+        <Typography variant="body1" style={{margin: '20px'}}>{data.explanation}</Typography>
+        <div className='photo'>
+          <CardMedia>
+
+           <img src={data.url} alt={data.title} />
+          </CardMedia>
+        </div>
       </div>
     </div>
     </>
